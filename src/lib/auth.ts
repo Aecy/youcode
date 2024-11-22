86e50dc2c9404f50bb063cc5d914aacb75cfdc5c
Aecy <devsnake@hotmail.com>
2024-11-22 21:26:05 +0100

    chore: setup next-auth

---
+++ src/lib/auth.ts
@@ -0,0 +1,12 @@
+import {GetServerSidePropsContext, NextApiRequest, NextApiResponse} from 'next';
+import {getServerSession} from 'next-auth';
+import {authOptions} from '@/app/api/auth/[...nextauth]';
+
+type ParametersGetServerSession =
+  | []
+  | [GetServerSidePropsContext['req'], GetServerSidePropsContext['res']]
+  | [NextApiRequest, NextApiResponse];
+
+export const getAuthSession = async (...parameters: ParametersGetServerSession) => {
+  return await getServerSession(...parameters, authOptions);
+};
